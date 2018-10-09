d0d1584318622ad6e40b6952e1fcf5286331f33a
pchla24 <janusz_sawicki97@o2.pl>
2018-10-09 21:27:04 +0200

    na szybko

---
+++ Wino/01-http.py
@@ -0,0 +1,19 @@
+from werkzeug.wrappers import Request, Response
+import os
+
+@Request.application
+def application(request):
+    html_file = open("index.html", "rb")
+    html_content = html_file.read()
+
+    css_file = open("static/style.css", "rb")
+    css_content = css_file.read()
+
+    image_file = open("static/wina.jpg", "rb")
+    image_content = image_file.read()
+
+    return Response(html_content, content_type="text/html", status='200 OK')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    app = run_simple('127.0.0.1', 4444, application, static_files={'/static':'static'}).wsgi_app()
