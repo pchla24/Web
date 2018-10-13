eaa9669c14caabb3ffc1e31ec5b8857f35084492
pchla24 <janusz_sawicki97@o2.pl>
2018-10-13 14:46:01 +0200

    Dodana siła hasła

--- Wino/app.js
+++ Wino/app.js
@@ -1,6 +1,8 @@
 var password = document.getElementById("password"),
 confirm_password = document.getElementById("confirm_password");
 
+var passText = document.getElementById("passText");
+
 function validatePassword() {
     if (password.value != confirm_password.value) {
         confirm_password.setCustomValidity("Hasła nie są identyczne");
@@ -9,5 +11,40 @@ function validatePassword() {
     }
 }
 
+function checkPassword() {
+    var strenghtBar = document.getElementById("passMeter")
+    var strength = 0;
+    if (password.value.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+        strength += 1
+    }
+    if (password.value.match(/[~<>?]+/)) {
+        strength += 1
+    }
+    if (password.value.match(/[!@$%^&*()]+/)) {
+        strength += 1
+    }
+    if (password.value.length > 5) {
+        strength += 1
+    }
+    switch(strength) {
+        case 0:
+            strenghtBar.value = 0;
+            break
+        case 1:
+            strenghtBar.value = 1;
+            break
+        case 2:
+            strenghtBar.value = 2;
+            break
+        case 3:
+            strenghtBar.value = 3;
+            break
+        case 4:
+            strenghtBar.value = 4;
+            break 
+    }
+}
+
+password.onkeyup = checkPassword;
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
