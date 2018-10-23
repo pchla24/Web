a91fb2ff48e83e14ee90482cd3825fb3c98b6d56
pchla24 <janusz_sawicki97@o2.pl>
2018-10-24 00:24:24 +0200

    Brakuje tylko uploadowania plików

--- FileHolder/z3.py
+++ FileHolder/z3.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask import session
 from flask import request
 from flask import render_template
+from flask import send_from_directory
 import os
 from os.path import expanduser
 from werkzeug import generate_password_hash, check_password_hash
 from flask import redirect
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = b'0329727 qwnsubnsb29'
@@ -66,28 +68,72 @@ def signIn():
         session['user'] = _login
         return redirect('/sawickij/z3/userHome')
 
-		return redirect('/sawickij/z3')
-        
+    return redirect('/sawickij/z3/login')
+
+
 
 @app.route('/sawickij/z3/userHome')
 def userHome():
     if session.get('user'):
-      return render_template('userHome.html')
+      username = session['user']
+      userpath = 'storage/' + username + '/'
+      userfiles = listUserFiles(username)
+
+      userfiles += [''] * (5 - len(userfiles))
+
+      path1 = ''
+      path2 = ''
+      path3 = ''
+      path4 = ''
+      path5 = ''
+
+      if len(userfiles) == 0:
+        pass
+      if len(userfiles) >= 1:
+        path1 = userpath + userfiles[0]
+      if len(userfiles) >= 2:
+        path2 = userpath + userfiles[1]
+      if len(userfiles) >= 3:
+        path3 = userpath + userfiles[2]
+      if len(userfiles) >= 4:
+        path4 = userpath + userfiles[3]
+      if len(userfiles) >= 5:
+        path5 = userpath + userfiles[4]
+      
+      return render_template('userHome.html', user=username, path1 = path1, path2 = path2,
+        path3 = path3, path4 = path4, path5 = path5, filename1 = userfiles[0], filename2 = userfiles[1],
+        filename3 = userfiles[2], filename4 = userfiles[3], filename5 = userfiles[4])
+
     else:
-			return render_template('index.html')
+      return redirect('/sawickij/z3')
+
        
 
 @app.route('/sawickij/z3/upload', methods=['POST'])
 def upload():
-    upload_folder = '/sawickij/z3/storage/' + session['user']
-    app.config['UPLOAD_FOLDER'] = upload_folder
-    file = request.files['file']
-    # Wywala przy wykonywaniu powyższej linii
-    #filename = secure_filename(file.filename)
-    #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect('/sawickij/z3/userHome')
+    username = session['user']
+    userpath = 'storage/' + username + '/'
+    userfiles = listUserFiles(username)
+    if len(userfiles) < 5:
+      for filee in request.files:
+        f = request.files[filee]
+        f.save(userpath + secure_filename(f.filename))
+      return redirect('/sawickij/z3/userHome')
+
+
+@app.route('/sawickij/z3/storage/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    return send_from_directory(directory='storage', filename=filename)
+
 
 @app.route('/sawickij/z3/logout')
 def logout():
     session.pop('user', None)
     return redirect('/sawickij/z3')
+
+def listUserFiles(username):
+    userpath = 'storage/' + username + '/'
+    return os.listdir(userpath)
+
+
+
